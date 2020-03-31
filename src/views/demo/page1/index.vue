22005dc51b2064356ddeacba580d1bbd115871b2
xujiahao <532107304@qq.com>
2020-03-31 16:59:18 +0800

    删除右上按钮

--- src/views/demo/page1/index.vue
+++ src/views/demo/page1/index.vue
@@ -1,7 +1,8 @@
 <template>
     <d2-container>
         <template slot="header">Page 1 header</template>
-        Hello World
+        Hello World是的
+        <template slot="footer">Page 1 header</template>
     </d2-container>
 </template>
 
